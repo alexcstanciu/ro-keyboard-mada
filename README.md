45325e0cf5409ed3a9dc8a60ff0e19856055843c
Alexandru Stanciu <alex@ici.ro>
2016-07-12 16:46:53 +0300

    fix install.sh

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Romanian Programmers Keyboard for Mac
 
-This keyboard layout provides Romanian special characters with alt(option) key.  
+This keyboard layout provides the Romanian special characters as a combination of alt(option) and a, q, s, i, t keys.
+
+Both `Romanian Programmers.icns` and `Romanian Programmers.keylayout` files should be copied to the `~/Library/Keyboard Layouts/` folder.
+
+Alternatively, this command can be run:
+
+```
+sh <(curl -sL https://github.com/alexcstanciu/ro-keyboard-mac/raw/master/install.sh)
+```
 
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -L https://github.com/alexcstanciu/ro-keyboard-mac/raw/master/Romanian%20Programmers.icns -o ~/Library/Keyboard\ Layouts/Romanian\ Programmers.icns
+curl -sL https://github.com/alexcstanciu/ro-keyboard-mac/raw/master/Romanian%20Programmers.icns -o ~/Library/Keyboard\ Layouts/Romanian\ Programmers.icns
 
-curl -L https://github.com/alexcstanciu/ro-keyboard-mac/raw/master/Romanian%20Programmers.keylayout -o ~/Library/Keyboard\ Layouts/Romanian\ Programmers.keylayout
+curl -sL https://github.com/alexcstanciu/ro-keyboard-mac/raw/master/Romanian%20Programmers.keylayout -o ~/Library/Keyboard\ Layouts/Romanian\ Programmers.keylayout
 
